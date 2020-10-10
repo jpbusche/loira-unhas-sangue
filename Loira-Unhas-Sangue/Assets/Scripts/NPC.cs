7428a1e1a1b7a6e9fae2fc9d114c878b7ec3be58
João Paulo Busche <joaopaulo.busche@gmail.com>
2020-10-10 19:50:20 -0300

    Add movement to player and NPC

---
+++ Loira-Unhas-Sangue/Assets/Scripts/NPC.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [SerializeField] float speed;
+    [SerializeField] Vector2 decisionTime;
+    float timeCount, horizontal, vertical;
+    Vector3 input, movement;
+    Rigidbody2D body;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        input = Vector3.zero;
+        timeCount = Random.Range(decisionTime.x, decisionTime.y);
+    }
+
+    void FixedUpdate() {
+        Debug.Log(timeCount);
+        if (timeCount > 0) timeCount -= Time.fixedDeltaTime;
+        else {
+            timeCount = Random.Range(decisionTime.x, decisionTime.y);
+            input.x = Random.Range(-1, 2);
+            input.y = Random.Range(-1, 2);
+            Rotation();
+        }
+        movement = input.normalized * speed * Time.fixedDeltaTime;
+        body.MovePosition(transform.position + movement);
+    }
+
+    void Rotation() {;
+        if (input.x == 1 && input.y == 0) transform.localRotation = Quaternion.Euler(0, 0, 90);
+        else if (input.x == -1 && input.y == 0) transform.localRotation = Quaternion.Euler(0, 0, 270);
+        else if (input.x == 0 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 180);
+        else if (input.x == 0 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 0);
+        else if (input.x == 1 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 45);
+        else if (input.x == 1 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 135);
+        else if (input.x == -1 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 225);
+        else if (input.x == -1 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 315);
+    }
+}
---
+++ Loira-Unhas-Sangue/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float speed;
+    Vector3 input, movement;
+    Rigidbody2D body;
+    
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        input = Vector3.zero;
+    }
+
+    void FixedUpdate() {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        Rotation();
+        movement = input.normalized * speed * Time.fixedDeltaTime;
+        body.MovePosition(transform.position + movement);
+    }
+
+    void Rotation() {;
+        if (input.x == 1 && input.y == 0) transform.localRotation = Quaternion.Euler(0, 0, 90);
+        else if (input.x == -1 && input.y == 0) transform.localRotation = Quaternion.Euler(0, 0, 270);
+        else if (input.x == 0 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 180);
+        else if (input.x == 0 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 0);
+        else if (input.x == 1 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 45);
+        else if (input.x == 1 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 135);
+        else if (input.x == -1 && input.y == 1) transform.localRotation = Quaternion.Euler(0, 0, 225);
+        else if (input.x == -1 && input.y == -1) transform.localRotation = Quaternion.Euler(0, 0, 315);
+    }
+}
